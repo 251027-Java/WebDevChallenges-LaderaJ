e2a0f43ae6680a12e6b694f388ac5130258acdbe
JB Ladera <minidomou@gmail.com>
2025-11-19 14:32:26 -0600

    add favorite color

--- t10/script.js
+++ t10/script.js
@@ -1,9 +1,13 @@
 function greetUser() {
     let name = document.getElementById("nameInput").value;
+    const color = document.getElementById('colorInput').value.trim();
+
+
     if (name.trim() === "") {
         alert("Please enter your name first!");
     } else {
-        alert("Hello, " + name + "!");
+        const extra = color.length ? ` Your favorite color is ${color}!` : '';
+        alert("Hello, " + name + "!" + extra);
     }
 }
 
